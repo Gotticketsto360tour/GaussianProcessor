7890cf88d19dd88b849721e5f5f968962e366679
gotticketsto360tour <werling1407@gmail.com>
2021-06-05 15:33:18 +0200

    multiplicative works!

--- GaussianProcessor.py
+++ GaussianProcessor.py
@@ -808,3 +808,4 @@ y_pred_test, sigma_test = gp.predict(x_test, return_std=True)
 # (22) Consider making dot product and dot product squared to have much lower weights, so that the kernel isn't dominated by them
 # (23) Make the learning mode have the same "pop-up" with kernels
 # (25) It is still extreeeemely volatile - even remotely large datasets breaks it. Write section about volatility as main part of future work.
+# (26) Insert multiplicative in simulated data and delete exp sine 2 everywhere.
